6e636e1ec792ca21c0f05f7bca07e4a8785ed19c
Joe Reiff <jcreiff@gmail.com>
2015-06-12 12:44:35 -0400

    change question type and required to dropdowns

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -4,7 +4,7 @@ class Survey < ActiveRecord::Base
   has_many :questions
   has_many :submissions
 
-  accepts_nested_attributes_for :questions, allow_destroy: true
+  accepts_nested_attributes_for :questions,  reject_if: :all_blank, allow_destroy: true
 
   validates :author_id, presence: true
   validates :title, presence: true
