66dcc0f94edd4615de726daddbcb99e36bf375bd
Joe Reiff <jcreiff@gmail.com>
2015-06-15 21:50:53 -0400

    edit survey model and tests

--- test/models/survey_test.rb
+++ test/models/survey_test.rb
@@ -1,21 +1,26 @@
 require 'test_helper'
 
 class SurveyTest < ActiveSupport::TestCase
-  test "knows which questions are required" do
+  setup do
     @survey = surveys(:one)
     @survey_two = surveys(:two)
     @question = questions(:three)
     @question_four = questions(:four)
+    @new_survey = Survey.create!(title: @survey.title, author: authors(:one))
+  end
 
+  test "knows which questions are required" do
     assert_equal [@question, @question_four], @survey.required_questions
     assert_equal [], @survey_two.required_questions
   end
 
   test "knows if it has questions" do
-    @survey = surveys(:one)
-    @new_survey = Survey.create!(title: @survey.title, author: authors(:one))
-
     assert_equal true, @survey.check_if_questions?
     assert_equal false, @new_survey.check_if_questions?
+  end
+
+  test "knows if it has submissions" do
+    assert_equal true, @survey.check_if_submissions?
+    assert_equal false, @new_survey.check_if_submissions?
   end
 end
